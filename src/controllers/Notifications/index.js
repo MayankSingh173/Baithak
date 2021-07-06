a25e6346223e8474c5a8f13daee6d3b380982101
Mayank Singh <mayankayush173@gmail.com>
2021-07-06 17:28:14 +0530

    added image notification

--- src/controllers/Notifications/index.js
+++ src/controllers/Notifications/index.js
@@ -7,12 +7,15 @@ const sendNotification = async (req, res) => {
       notification: {
         title: payload.title,
         body: payload.body,
-        imageUrl: payload.imageUrl,
+      },
+      data: {
+        data: JSON.stringify(payload.data),
       },
       tokens: payload.tokens,
       android: {
         notification: {
           color: '#45F1DE',
+          imageUrl: payload.imageUrl,
         },
       },
     };
