ec2912e89e73cfd03f2f9ef3f4f26837a493385d
Mayank Singh <mayankayush173@gmail.com>
2021-07-11 22:20:17 +0530

    added onboarding screens

--- src/navigation/AuthStackNavigator/AuthStackNavigator.tsx
+++ src/navigation/AuthStackNavigator/AuthStackNavigator.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {
   FORGOT_PASSWORD_SCREEN,
+  ONBOARDING_SCREEN_NAME,
   SIGN_IN_SCREEN,
   SIGN_UP_SCREEN,
 } from '../../constants/Navigation/Navigation';
 import SignIn from '../../screens/Auth/SignIn/SignIn';
 import SignUp from '../../screens/Auth/SignUp/SignUp';
 import ForgotPassword from '../../screens/Auth/ForgotPassword/ForgotPassword';
+import OnboardingNavigator from '../OnboardingNavigator/OnboardingNavigator';
 
 const Stack = createStackNavigator();
 
 const AuthStackNavigator = () => {
   return (
     <Stack.Navigator headerMode="none">
+      <Stack.Screen
+        name={ONBOARDING_SCREEN_NAME}
+        component={OnboardingNavigator}
+      />
       <Stack.Screen name={SIGN_IN_SCREEN} component={SignIn} />
       <Stack.Screen name={SIGN_UP_SCREEN} component={SignUp} />
       <Stack.Screen name={FORGOT_PASSWORD_SCREEN} component={ForgotPassword} />
